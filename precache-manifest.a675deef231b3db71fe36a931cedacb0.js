aa8d12baf5deeb70a88848a482e2d9cbc93ac910
ArulFD <41385461+ArulFD@users.noreply.github.com>
2019-11-01 21:32:40 +0530

    initial commit
    initial commit

---
+++ precache-manifest.a675deef231b3db71fe36a931cedacb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62fc6c1dd1ac21b929fe7464343b612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47f7c4f201ce3b7a620b",
+    "url": "/static/css/2.7f36afd0.chunk.css"
+  },
+  {
+    "revision": "d08d4f5ec47ef343cfd0",
+    "url": "/static/css/main.a53ec1b3.chunk.css"
+  },
+  {
+    "revision": "47f7c4f201ce3b7a620b",
+    "url": "/static/js/2.b507f8cd.chunk.js"
+  },
+  {
+    "revision": "d08d4f5ec47ef343cfd0",
+    "url": "/static/js/main.fabe1c36.chunk.js"
+  },
+  {
+    "revision": "8724ae32eb07bece4508",
+    "url": "/static/js/runtime-main.326c3d5b.js"
+  }
+]);
